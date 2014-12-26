a844c8532e5c8c6bd155de81c24cba69577ba00b
Benjamin Jordan <bmj465@gmail.com>
2014-12-26 14:57:42 -0600

    Archive was being stored in wrong directory.

--- ebtask_example.bat
+++ ebtask_example.bat
@@ -7,4 +7,4 @@ set PASSWORD=[Password (application specific password for gmail accounts)]
 set IMAP=[IMAP server address]
 
 %python% %BASE_DIR%\ebackup.py -e %EMAIL% -p %PASSWORD% -i %IMAP% -o %OUTPUT_DIR%
-%python% %BASE_DIR%\ebexporter.py -i %OUTPUT_DIR%\%EMAIL% -o %OUTPUT_DIR%\Archive.mbox
+%python% %BASE_DIR%\ebexporter.py -i %OUTPUT_DIR%\%EMAIL% -o %OUTPUT_DIR%\%EMAIL%\Archive.mbox
